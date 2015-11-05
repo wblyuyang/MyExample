8ae44069e5d5bcc72830b53de751cfa71bfeccda
wblyuyang <android_yy@foxmail.com>
2015-11-05 18:43:50 +0800

    wblyuyang

---
+++ src/main/java/com/yy/example/NewCreate.java
@@ -0,0 +1,15 @@
+package com.yy.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by YY on 2015/11/5.
+ */
+public class NewCreate extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
